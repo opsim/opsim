e9175c00210e26d2ba3be1c79e8bed08935a65a6
samuelcartaxo <samuelcartaxo@68d02639-a20e-0410-a776-d98cfde40b44>
2006-03-19 17:43:54 +0000

    +Unit for general utilities routines.

---
+++ src/ise/Utils.pas
@@ -0,0 +1,40 @@
+{
+ *****************************************************************************
+ *                                                                           *
+ *  This file is part of the OpSim - OPEN SOURCE PROCESS SIMULATOR           *
+ *                                                                           *
+ *  See the file COPYING.GPL, included in this distribution,                 *
+ *  for details about the copyright.                                         *
+ *                                                                           *
+ *  This program is distributed in the hope that it will be useful,          *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                     *
+ *                                                                           *
+ *****************************************************************************
+}
+unit Utils;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils;
+
+  function IsInvalidPoint(P: TPoint): Boolean;
+  procedure SetInvalidPoint(var P: TPoint);
+
+implementation
+
+function IsInvalidPoint(P: TPoint): Boolean;
+begin
+  Result := (P.X = -1) and (P.Y = -1);
+end;
+
+procedure SetInvalidPoint(var P: TPoint);
+begin
+  P := Point(-1,-1);
+end;
+
+end.
+
