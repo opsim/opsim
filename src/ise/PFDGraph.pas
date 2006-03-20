9c2fd6c297029c870c70d6529cef2661fad9d3d6
samuelcartaxo <samuelcartaxo@68d02639-a20e-0410-a776-d98cfde40b44>
2006-03-20 21:10:57 +0000

    +Create unit PFDGraph.pas: groups routines for rendering the PFD objects.

---
+++ src/ise/PFDGraph.pas
@@ -0,0 +1,38 @@
+{ $Id$ }
+{
+ /***************************************************************************
+                               PFDGraph.pas
+                               ------------
+                  Abstract: Groups routines for rendering the PFD objects.
+                  Initial Revision : 20/03/2006
+                  Author: Samuel Jorge Marques Cartaxo
+
+
+ ***************************************************************************/
+
+ *****************************************************************************
+ *                                                                           *
+ *  This file is part of the OpSim - OPEN SOURCE PROCESS SIMULATOR           *
+ *                                                                           *
+ *  See the file COPYING.GPL, included in this distribution,                 *
+ *  for details about the copyright.                                         *
+ *                                                                           *
+ *  This program is distributed in the hope that it will be useful,          *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                     *
+ *                                                                           *
+ *****************************************************************************
+}
+unit PFDGraph;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils;
+
+implementation
+
+end.
+
