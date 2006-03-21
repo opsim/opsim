6e7f6642a5bf3273b09439eef99bf66317270abf
samuelcartaxo <samuelcartaxo@68d02639-a20e-0410-a776-d98cfde40b44>
2006-03-21 02:43:48 +0000

    +Defining names and hints.

--- src/ise/UnitopPalletU.pas
+++ src/ise/UnitopPalletU.pas
@@ -32,7 +32,7 @@ interface
 
 uses
   Classes, SysUtils, LResources, Forms, Controls, Graphics, Dialogs, ExtCtrls,
-  Buttons;
+  Buttons, XMLPropStorage, IniPropStorage;
 
 type
 
@@ -43,31 +43,32 @@ type
     pnlToolbar1: TPanel;
     pnlToolbar2: TPanel;
     pnlToolbar3: TPanel;
-    SpeedButton1: TSpeedButton;
-    SpeedButton10: TSpeedButton;
-    SpeedButton11: TSpeedButton;
-    SpeedButton12: TSpeedButton;
+    sbCSTR: TSpeedButton;
+    sbTee: TSpeedButton;
+    sbHeatExchanger: TSpeedButton;
+    sbTank: TSpeedButton;
     SpeedButton13: TSpeedButton;
     SpeedButton14: TSpeedButton;
     SpeedButton15: TSpeedButton;
-    SpeedButton16: TSpeedButton;
-    SpeedButton17: TSpeedButton;
-    SpeedButton18: TSpeedButton;
-    SpeedButton19: TSpeedButton;
-    SpeedButton2: TSpeedButton;
+    sbSeparator: TSpeedButton;
+    sbValve: TSpeedButton;
+    sbCooler: TSpeedButton;
+    sbPipeSegment: TSpeedButton;
+    sbCompressor: TSpeedButton;
     SpeedButton20: TSpeedButton;
     SpeedButton21: TSpeedButton;
-    SpeedButton22: TSpeedButton;
-    SpeedButton23: TSpeedButton;
+    sbExpander: TSpeedButton;
+    sb3PhaseSeparator: TSpeedButton;
     SpeedButton24: TSpeedButton;
-    SpeedButton3: TSpeedButton;
-    SpeedButton4: TSpeedButton;
+    sbHeater: TSpeedButton;
+    sbPump: TSpeedButton;
     SpeedButton5: TSpeedButton;
     SpeedButton6: TSpeedButton;
     SpeedButton7: TSpeedButton;
-    SpeedButton8: TSpeedButton;
-    SpeedButton9: TSpeedButton;
+    sbMixer: TSpeedButton;
+    sbPFR: TSpeedButton;
     procedure FormCreate(Sender: TObject);
+    procedure XMLPropStorage1SavingProperties(Sender: TObject);
   end;
   
 var
@@ -91,6 +92,11 @@ begin
     MinWidth := Width;
     MaxWidth := Width;
   end;//with
+end;
+
+procedure TUnitopPallet.XMLPropStorage1SavingProperties(Sender: TObject);
+begin
+
 end;
 
 initialization
