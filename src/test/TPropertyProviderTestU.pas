e1224b7b75322c893575f1ed28285b216a013753
samuelcartaxo <samuelcartaxo@68d02639-a20e-0410-a776-d98cfde40b44>
2006-04-02 20:52:46 +0000

    Routine for testing the addition functionality of TPropertyProvider. Test OK.

--- src/test/TPropertyProviderTestU.pas
+++ src/test/TPropertyProviderTestU.pas
@@ -30,16 +30,93 @@ unit TPropertyProviderTestU;
 interface
 
 uses
-  Classes, SysUtils, LResources, Forms, Controls, Graphics, Dialogs;
+  Classes, SysUtils, LResources, Forms, Controls, Graphics, Dialogs, ExtCtrls,
+  ComCtrls, StdCtrls, Buttons, ThermoProvider;
 
 type
+
+  { TfmTPropertyProviderTest }
+
   TfmTPropertyProviderTest = class (TForm)
+    btnAdd: TButton;
+    btnClearCompsList: TButton;
+    Button2: TButton;
+    edCompID: TEdit;
+    GroupBox1: TGroupBox;
+    Label1: TLabel;
+    lstbxComps: TListBox;
+    PageControl1: TPageControl;
+    Panel1: TPanel;
+    TabSheet1: TTabSheet;
+    TabSheet2: TTabSheet;
+    procedure btnAddClick(Sender: TObject);
+    procedure btnClearCompsListClick(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
+  private
+    PropertyProvider: TPropertyProvider;
+    procedure UpdateList;
   end;
   
 var
   fmTPropertyProviderTest: TfmTPropertyProviderTest;
 
 implementation
+
+uses
+  DMBaseU;
+
+{ TfmTPropertyProviderTest }
+
+{
+*************************** TfmTPropertyProviderTest ***************************
+}
+procedure TfmTPropertyProviderTest.btnAddClick(Sender: TObject);
+begin
+  //Add the componenent.
+  with PropertyProvider do begin
+    AddCompound(edCompID.Text);
+  end;//with
+  UpdateList;
+end;
+
+procedure TfmTPropertyProviderTest.btnClearCompsListClick(Sender: TObject);
+begin
+  with PropertyProvider do begin
+    Compounds.Clear;
+  end;//with
+  UpdateList;
+end;
+
+procedure TfmTPropertyProviderTest.Button2Click(Sender: TObject);
+begin
+  Close;
+end;
+
+procedure TfmTPropertyProviderTest.FormCreate(Sender: TObject);
+begin
+  PropertyProvider := TPropertyProvider.Create;
+  with PropertyProvider do begin
+    DBConnection := DMBase.DbConnectionMaster;
+  end//with
+end;
+
+procedure TfmTPropertyProviderTest.FormDestroy(Sender: TObject);
+begin
+  PropertyProvider.Free;
+end;
+
+procedure TfmTPropertyProviderTest.UpdateList;
+var
+  I: Integer;
+begin
+  with PropertyProvider do begin
+    lstbxComps.Items.Clear;
+    for I := 0 to Compounds.Count - 1 do
+      lstbxComps.Items.AddObject(Compounds[I].CompName, Compounds[I]);
+  end;//with
+end;
 
 initialization
 
