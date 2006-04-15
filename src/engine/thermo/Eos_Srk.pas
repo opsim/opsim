105967fdb9a74ee98b8a3eed59c5a1b492a265fd
samuelcartaxo <samuelcartaxo@68d02639-a20e-0410-a776-d98cfde40b44>
2006-04-15 19:31:25 +0000

    Initiating SRK implementation.

---
+++ src/engine/thermo/Eos_Srk.pas
@@ -0,0 +1,42 @@
+{ $Id$ }
+{
+ /***************************************************************************
+
+                  Abstract: Implementation of SRK equation of state.
+                  Initial Revision : 15/04/2006
+                  Authors:
+                    - Samuel Jorge Marques Cartaxo
+                    - Matt Henley
+                    - Additional contributors...
+
+ ***************************************************************************/
+
+ *****************************************************************************
+ *                                                                           *
+ *  This file is part of the OpSim - OPEN SOURCE PROCESS SIMULATOR           *
+ *                                                                           *
+ *  See the file COPYING.GPL, included in this distribution,                 *
+ *  for details about the copyright.                                         *
+ *                                                                           *
+ *  This program is distributed in the hope that it will be useful,          *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                     *
+ *                                                                           *
+ *****************************************************************************
+}
+unit Eos_Srk;
+
+interface
+
+uses
+  SysUtils, Classes, Entities, Thermo;
+
+type
+  TSrkEos = class (TEos)
+  end;
+  
+
+implementation
+
+
+end.
