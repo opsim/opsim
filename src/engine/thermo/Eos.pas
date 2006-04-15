1cda616137b4c3525c3f9877f587bf057f3f5a5a
samuelcartaxo <samuelcartaxo@68d02639-a20e-0410-a776-d98cfde40b44>
2006-04-15 18:13:06 +0000

    Unit for base classes of equations of state.

---
+++ src/engine/thermo/Eos.pas
@@ -0,0 +1,58 @@
+{ $Id$ }
+{
+ /***************************************************************************
+
+                  Abstract: Base classes for equation of state.
+                  Initial Revision : 15/04/2006
+                  Authors:
+                    - Samuel Jorge Marques Cartaxo
+                    - Additional contributors...
+
+ ***************************************************************************/
+
+ *****************************************************************************
+ *                                                                           *
+ *  This file is part of the OpSim - OPEN SOURCE PROCESS SIMULATOR           *
+ *                                                                           *
+ *  See the file COPYING.GPL, included in this distribution,                 *
+ *  for details about the copyright.                                         *
+ *                                                                           *
+ *  This program is distributed in the hope that it will be useful,          *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                     *
+ *                                                                           *
+ *****************************************************************************
+}
+unit Eos;
+
+interface
+
+uses
+  SysUtils, Classes;
+
+type
+  {{
+  Base class for all equations of state.
+  }
+  TEos = class (TObject)
+  public
+    function CalcCompressibilityFactor(APhase: TPhase): TValueRec;
+    function CalcFugacityCoefficient(ACompounds: TCompounds): TValueRec;
+  end;
+  
+
+implementation
+
+{
+************************************* TEos *************************************
+}
+function TEos.CalcCompressibilityFactor(APhase: TPhase): TValueRec;
+begin
+end;
+
+function TEos.CalcFugacityCoefficient(ACompounds: TCompounds): TValueRec;
+begin
+end;
+
+
+end.
