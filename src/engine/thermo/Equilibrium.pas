47ebe8d6764ebed79950f982727c1cbf9afc4170
samuelcartaxo <samuelcartaxo@68d02639-a20e-0410-a776-d98cfde40b44>
2006-04-21 14:13:37 +0000

    Module for equilibrium services.

---
+++ src/engine/thermo/Equilibrium.pas
@@ -0,0 +1,47 @@
+{ $Id$ }
+{
+ /***************************************************************************
+
+                  Abstract: Classes and routines related to equilibrium
+                            calculations.
+                  Initial Revision : 21/04/2006
+                  Authors:
+                    - Samuel Jorge Marques Cartaxo
+                    - Matt Henley
+                    - Hazem Haddad
+                    - Additional contributors...
+
+ ***************************************************************************/
+
+ *****************************************************************************
+ *                                                                           *
+ *  This file is part of the OpSim - OPEN SOURCE PROCESS SIMULATOR           *
+ *                                                                           *
+ *  See the file COPYING.GPL, included in this distribution,                 *
+ *  for details about the copyright.                                         *
+ *                                                                           *
+ *  This program is distributed in the hope that it will be useful,          *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                     *
+ *                                                                           *
+ *****************************************************************************
+}
+unit Equilibrium;
+
+interface
+
+uses
+  SysUtils, Classes, Entities, Thermo;
+
+type
+  TEquilibriumServer = class (TObject)
+  end;
+  
+
+type
+
+implementation
+
+
+
+end.
