a8ead1d017693bea09c32231173047b13e83f1bf
samuelcartaxo <samuelcartaxo@68d02639-a20e-0410-a776-d98cfde40b44>
2006-04-24 02:54:01 +0000

    Tests for property provider.

--- src/test/TPropertyProviderTestCaseU.pas
+++ src/test/TPropertyProviderTestCaseU.pas
@@ -30,20 +30,31 @@ unit TPropertyProviderTestCaseU;
 interface
 
 uses
-  Classes, SysUtils, fpcunit, testutils, testregistry, ZConnection;
+  Classes, SysUtils, fpcunit, testutils, testregistry, ZConnection,
+  ThermoProvider;
 
 type
 
   TPropertyProviderTestCase = class (TTestCase)
   protected
     DbConnectionMaster: TZConnection;
+    PropertyProvider: TPropertyProvider;
     procedure SetUp; override;
     procedure TearDown; override;
   published
+    procedure TestAdd;
+    procedure TestClear;
     procedure TestDbConnection;
+    procedure TestDelete;
+    procedure TestFindCompound;
   end;
   
 implementation
+
+const
+  cCompID1 = 11;
+  cCompID2 = 22;
+  cCompID3 = 33;
 
 {
 ************************** TPropertyProviderTestCase ***************************
@@ -59,11 +70,35 @@ begin
     Password := 'masterkey';
     Connected := True;
   end;//with
+  PropertyProvider := TPropertyProvider.Create;
+  with PropertyProvider do begin
+    DBConnection := DbConnectionMaster;
+  end//with
 end;
 
 procedure TPropertyProviderTestCase.TearDown;
 begin
   DbConnectionMaster.Free;
+  PropertyProvider.Free;
+end;
+
+procedure TPropertyProviderTestCase.TestAdd;
+begin
+  with PropertyProvider do begin
+    AddCompound(cCompID1);
+    AddCompound(cCompID2);
+    AddCompound(cCompID3);
+    AssertEquals(Compounds.Count, 3);
+  end;//with
+end;
+
+procedure TPropertyProviderTestCase.TestClear;
+begin
+  TestAdd;
+  with PropertyProvider do begin
+    Compounds.Clear;
+    AssertEquals(Compounds.Count, 0);
+  end;//with
 end;
 
 procedure TPropertyProviderTestCase.TestDbConnection;
@@ -73,8 +108,36 @@ begin
   AssertEquals(DbConnectionMaster.Connected, True);
 end;
 
-Initialization
+procedure TPropertyProviderTestCase.TestDelete;
+begin
+  TestAdd;
+  with PropertyProvider do begin
+    DeleteCompound(cCompID2);
+    AssertEquals(Compounds.Count, 2);
+    AssertEquals(FindCompound(cCompID2) = nil, True);
+  end;//with
+end;
 
+procedure TPropertyProviderTestCase.TestFindCompound;
+begin
+  with PropertyProvider do begin
+    AssertEquals(FindCompound(cCompID2) <> nil, False);
+    TestAdd;
+    AssertEquals(FindCompound(cCompID1) <> nil, True);
+    AssertEquals(FindCompound(cCompID2) <> nil, True);
+    AssertEquals(FindCompound(cCompID3) <> nil, True);
+    DeleteCompound(cCompID2);
+    AssertEquals(FindCompound(cCompID1) <> nil, True);
+    AssertEquals(FindCompound(cCompID2) <> nil, False);
+    AssertEquals(FindCompound(cCompID3) <> nil, True);
+    Compounds.Clear;
+    AssertEquals(FindCompound(cCompID1) <> nil, False);
+    AssertEquals(FindCompound(cCompID2) <> nil, False);
+    AssertEquals(FindCompound(cCompID3) <> nil, False);
+  end;//with
+end;
+
+Initialization
   RegisterTest(TPropertyProviderTestCase);
 end.
 
