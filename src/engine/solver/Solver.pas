73bdb7af593f05618b0ef428160532e84873d87c
samuelcartaxo <samuelcartaxo@68d02639-a20e-0410-a776-d98cfde40b44>
2006-05-23 03:42:19 +0000

    Specialized equations classes.

--- src/engine/solver/Solver.pas
+++ src/engine/solver/Solver.pas
@@ -52,6 +52,39 @@ type
     property Variables: TList read FVariables write FVariables;
   end;
   
+  {{
+  - Defines a linear system of equations.
+  }
+  TAlgebraicEquation = class (TEquation)
+  end;
+  
+  {{
+  - Inherits from class TAlgebraicEquation and refines it to define a mixed set 
+  of differential and algebraic equations.
+  }
+  TDifferentialAlgebraicEquation = class (TAlgebraicEquation)
+  end;
+  
+  {{
+  - The definition of large sets of nonlinear equations of any kind generally 
+  requires a large amount of relatively complex data. This has led us to 
+  introduce the concept of an Equation Set Object (ESO) as a means of defining 
+  this information in a way that can be accessed and used by instances of 
+  NLASolvers and DAESolvers. The structure of the ESO is, therefore, central to 
+  the interface definitions which are the ultimate goal of this work. The 
+  Equation Set Object is an abstraction representing a square or rectangular 
+  set of equations. These are the equations that define the physical behaviour 
+  of the process1 under consideration, and which must be solved within a 
+  flowsheeting problem. The interface to this object is intended to serve the 
+  needs of the various solver objects by allowing them to obtain information 
+  about the size and structure of the system, to adjust the values of variables 
+  occurring in it, and to compute the resulting equation residuals and, 
+  potentially, other related information (e.g. partial der
+  ivatives). Hence, this interface requires standardisation as part of 
+  CAPE-OPEN. However, the construction of such an object will be a proprietary 
+  matter for individual vendors of flowsheeting packages and will be 
+  standardised as part of CAPE-OPEN.
+  }
   TEquations = class (TCollection)
   end;
   
