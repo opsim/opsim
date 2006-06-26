872dde609e862fd3dc0b3bb757391e888b54a163
samuelcartaxo <samuelcartaxo@68d02639-a20e-0410-a776-d98cfde40b44>
2006-06-26 16:58:43 +0000

    Created unit tests for TMaterial class.

--- src/test/TThermoTestCaseU.pas
+++ src/test/TThermoTestCaseU.pas
@@ -49,6 +49,20 @@ type
     procedure TestFindCompound;
   end;
   
+  TMaterialTestCase = class (TTestCase)
+  private
+    Material: TMaterial;
+  protected
+    PropertyProvider: TPropertyProvider;
+    procedure SetUp; override;
+    procedure TearDown; override;
+  published
+    procedure TestAddCompounds;
+    procedure TestAssignCompounds;
+    procedure TestClearCompounds;
+    procedure TestFindCompounds;
+  end;
+  
 implementation
 
 const
@@ -143,7 +157,109 @@ begin
   end;//with
 end;
 
+{
+****************************** TMaterialTestCase *******************************
+}
+procedure TMaterialTestCase.SetUp;
+begin
+  PropertyProvider := TPropertyProvider.Create;
+  //Sets a provider with a few compounds loaded.
+  with PropertyProvider do begin
+    DBConnection := GlobalDatabaseConnection;
+    AddCompound(cCompID1);
+    AddCompound(cCompID2);
+    AddCompound(cCompID3);
+    //AssertEquals(Compounds.Count, 3);
+  end;//with
+  Material := TMaterial.Create;
+end;
+
+procedure TMaterialTestCase.TearDown;
+begin
+  PropertyProvider.Free;
+  Material.Free;
+end;
+
+procedure TMaterialTestCase.TestAddCompounds;
+  
+  const
+    N = 5;
+  var
+    I: Integer;
+  
+begin
+  with Material.Compounds do begin
+    Clear;
+    for I := 1 to 5 do Add;
+    AssertEquals(Count, N);
+  end;//with
+end;
+
+procedure TMaterialTestCase.TestAssignCompounds;
+var
+  I: Integer;
+begin
+  //Define Compounds array.
+  with Material.Compounds do begin
+    Clear;
+    Assign(PropertyProvider.Compounds);
+    //Check total count.
+    AssertEquals(Count, PropertyProvider.Compounds.Count);
+    //Check a few compound properties.
+    for I := 0 to PropertyProvider.Compounds.Count - 1 do begin
+      AssertEquals(Items[I].CompID, PropertyProvider.Compounds[I].CompID);
+      AssertEquals(Items[I].CompName, PropertyProvider.Compounds[I].CompName);
+    end;//for
+  end;//with
+  
+  //Define Compositions array.
+  with Material.Compositions do begin
+    //Check total count of both arrays.
+    AssertEquals(Count, Material.Compounds.Count);
+    //Check a few compound properties.
+    for I := 0 to Count - 1 do begin
+      Items[I].MoleFraction.Value := Random;
+      Items[I].MassFraction.Value := Random;
+      Items[I].MoleFlow.Value := Random(100);
+      Items[I].MassFlow.Value := Random(100);
+    end;//for
+  end;//with
+end;
+
+procedure TMaterialTestCase.TestClearCompounds;
+begin
+  TestAddCompounds;
+  with Material.Compounds do begin
+    Clear;
+    AssertEquals(Count, 0);
+  end;//with
+end;
+
+procedure TMaterialTestCase.TestFindCompounds;
+begin
+  with Material.Compounds do begin
+    Clear;
+    AssertEquals(FindCompound(cCompID2) <> nil, False);
+    //Copy compounds from provider.
+    TestAssignCompounds;
+    AssertEquals(FindCompound(cCompID1) <> nil, True);
+    AssertEquals(FindCompound(cCompID2) <> nil, True);
+    AssertEquals(FindCompound(cCompID3) <> nil, True);
+    //Delete one compound and checks deletion.
+    DeleteCompound(cCompID2);
+    AssertEquals(FindCompound(cCompID1) <> nil, True);
+    AssertEquals(FindCompound(cCompID2) <> nil, False);
+    AssertEquals(FindCompound(cCompID3) <> nil, True);
+    //Delete all and check.
+    Clear;
+    AssertEquals(FindCompound(cCompID1) <> nil, False);
+    AssertEquals(FindCompound(cCompID2) <> nil, False);
+    AssertEquals(FindCompound(cCompID3) <> nil, False);
+  end;//with
+end;
+
 Initialization
   RegisterTest(TCompoundsTestCase);
+  RegisterTest(TMaterialTestCase);
 end.
 
