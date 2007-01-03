768a4831918515242824540b07cf2f7fd4346b9e
samuelcartaxo <samuelcartaxo@68d02639-a20e-0410-a776-d98cfde40b44>
2007-01-03 20:20:02 +0000

    Resolved incompatibilities with latest Lazarus/fpc releases. Still getting an access violation when moving control inside the PDF designer. Changes in the LCL treatment of messages have broken the prior code.

--- src/ise/PFDCtrls.pas
+++ src/ise/PFDCtrls.pas
@@ -30,7 +30,7 @@ interface
 
 uses
   SysUtils, Forms, Classes, Dialogs, Graphics, Controls, ExtCtrls, StdCtrls,
-  Types, Math, PFDGraph, LCLProc, LMessages, GraphMath;
+  Types, Math, PFDGraph, LCLProc, LMessages, GraphMath, LCLMessageGlue;
 
 type
 
@@ -366,7 +366,8 @@ begin
   //Needs to notify the mouse event over the control in order the PFDWorkplace
   //update drawings on its canvas.
   P := Parent.ScreenToClient(ClientToScreen(Point(X,Y)));
-  Parent.MouseDown(Button, Shift, P.X, P.Y);
+  //Parent.MouseDown(Button, Shift, P.X, P.Y);
+  LCLSendMouseDownMsg(Parent, P.X, P.Y, Button, Shift);
 end;
 
 procedure TPFDControl.MouseEnter;
@@ -398,7 +399,8 @@ begin
   //Needs to notify mouse movements over the control in order the PFDWorkplace
   //update drawings on its canvas.
   P := Parent.ScreenToClient(ClientToScreen(Point(X,Y)));
-  Parent.MouseMove(Shift, P.X, P.Y);
+  //Parent.MouseMove(Shift, P.X, P.Y);
+  LCLSendMouseMoveMsg(Parent, P.X, P.Y, Shift);
 end;
 
 procedure TPFDControl.MouseUp(Button: TMouseButton ; Shift: TShiftState; X, Y: 
@@ -410,7 +412,8 @@ begin
   //Needs to notify the mouse event over the control in order the PFDWorkplace
   //update drawings on its canvas.
   P := Parent.ScreenToClient(ClientToScreen(Point(X,Y)));
-  Parent.MouseUp(Button, Shift, P.X, P.Y);
+  //Parent.MouseUp(Button, Shift, P.X, P.Y);
+  LCLSendMouseUpMsg(Parent, P.X, P.Y, Button, Shift);
 end;
 
 procedure TPFDControl.Paint;
