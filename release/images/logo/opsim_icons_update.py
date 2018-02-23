079a0d3387581360cadadc1d88d02210fec15b97
daar <darius@opsim.cc>
2018-02-23 22:27:28 +0100

    add python script to generate the bitmap files delete all bitmap files, no need to keep them in GIT any more

---
+++ release/images/logo/opsim_icons_update.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+
+# This script updates opsim bitmap files from the SVG files
+import os
+import subprocess
+import sys
+
+def run(cmd):
+    print("   ", " ".join(cmd))
+    subprocess.check_call(cmd)
+
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+
+inkscape_bin = "inkscape"
+
+if sys.platform == 'darwin':
+    inkscape_app_path = '/Applications/Inkscape.app/Contents/Resources/script'
+    if os.path.exists(inkscape_app_path):
+        inkscape_bin = inkscape_app_path
+
+cmd = (
+    inkscape_bin,
+    os.path.join(BASEDIR, "OpSim.svg"),
+    "--export-width=16",
+    "--export-height=16",
+    "--without-gui",
+    "--export-png=" + os.path.join(BASEDIR, "opsim16.png"),
+)
+run(cmd)
+
+cmd = (
+    inkscape_bin,
+    os.path.join(BASEDIR, "OpSim.svg"),
+    "--export-width=32",
+    "--export-height=32",
+    "--without-gui",
+    "--export-png=" + os.path.join(BASEDIR, "opsim32.png"),
+)
+run(cmd)
+
+cmd = (
+    inkscape_bin,
+    os.path.join(BASEDIR, "OpSim.svg"),
+    "--export-width=48",
+    "--export-height=48",
+    "--without-gui",
+    "--export-png=" + os.path.join(BASEDIR, "opsim48.png"),
+)
+run(cmd)
+
+cmd = (
+    inkscape_bin,
+    os.path.join(BASEDIR, "OpSim.svg"),
+    "--export-width=64",
+    "--export-height=64",
+    "--without-gui",
+    "--export-png=" + os.path.join(BASEDIR, "opsim64.png"),
+)
+run(cmd)
+
+cmd = (
+    inkscape_bin,
+    os.path.join(BASEDIR, "OpSim.svg"),
+    "--export-width=128",
+    "--export-height=128",
+    "--without-gui",
+    "--export-png=" + os.path.join(BASEDIR, "opsim128.png"),
+)
+run(cmd)
+
+cmd = (
+    inkscape_bin,
+    os.path.join(BASEDIR, "OpSim.svg"),
+    "--export-width=256",
+    "--export-height=256",
+    "--without-gui",
+    "--export-png=" + os.path.join(BASEDIR, "opsim256.png"),
+)
+run(cmd)
+
+
+cmd = (
+    inkscape_bin,
+    os.path.join(BASEDIR, "logo.svg"),
+    "--export-width=201",
+    "--export-height=61",
+    "--without-gui",
+    "--export-png=" + os.path.join(BASEDIR, "logo.png"),
+)
+run(cmd)
